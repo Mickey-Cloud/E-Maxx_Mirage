15f818c4034b5098fc2222bc34d45c475b8aa877
Mickey-Cloud <u1117450@umail.utah.edu>
2024-06-04 21:45:52 -0600

    got a C++ script to render a texture

---
+++ VR Unity Project/nothing/Assets/scripts/Test.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Test : MonoBehaviour
+{
+    Texture2D _texture;
+    CommandBuffer _command;
+
+#if PLATFORM_IOS
+    [System.Runtime.InteropServices.DllImport("__Internal")]
+#else
+    [System.Runtime.InteropServices.DllImport("NativePlugin")]
+#endif
+    static extern System.IntPtr GetTextureUpdateCallback();
+
+    void Start()
+    {
+        _command = new CommandBuffer();
+        _texture = new Texture2D(64, 64, TextureFormat.RGBA32, false);
+        _texture.wrapMode = TextureWrapMode.Clamp;
+
+        // Set the texture to the renderer with using a property block.
+        var prop = new MaterialPropertyBlock();
+        prop.SetTexture("_MainTex", _texture);
+        GetComponent<Renderer>().SetPropertyBlock(prop);
+    }
+
+    void OnDestroy()
+    {
+        _command.Dispose();
+        Destroy(_texture);
+    }
+
+    void Update()
+    {
+        Debug.Log("here am I");
+        // Request texture update via the command buffer.
+        _command.IssuePluginCustomTextureUpdateV2(
+            GetTextureUpdateCallback(), _texture, (uint)(Time.time * 60)
+        );
+        Graphics.ExecuteCommandBuffer(_command);
+        _command.Clear();
+
+        Debug.Log("Rotate");
+        // Rotation
+        transform.eulerAngles = new Vector3(10, 20, 30) * Time.time;
+    }
+}
--- VR Unity Project/nothing/NativePlugin/NativePlugin.h
+++ VR Unity Project/nothing/NativePlugin/NativePlugin.h
@@ -1,9 +1,17 @@
 #ifndef NATIVEPLUGIN_H
 #define NATIVEPLUGIN_H
 
+#include "Unity/IUnityGraphics.h"
+
+#include <stddef.h>
+
+struct IUnityInterfaces;
+
 #define DLL_EXPORT _declspec(dllexport)
 
 extern "C" {
 	int DLL_EXPORT SimpleReturnFun();
+
+	UnityRenderingEventAndData UNITY_INTERFACE_EXPORT GetTextureUpdateCallback();
 }
 #endif
