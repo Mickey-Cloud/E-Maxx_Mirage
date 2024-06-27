fb0a94e588ebd05a6109568d7c3702f05b70a173
murphadonut <murphadonut@gmail.com>
2024-06-26 21:30:34 -0600

    updated unity transmit file

--- VR Unity Project/nothing/Assets/transmit.cs
+++ VR Unity Project/nothing/Assets/transmit.cs
@@ -14,6 +14,7 @@ public class Transmit : MonoBehaviour
     private NetworkStream stream;
     private Thread clientThread;
     private bool isRunning = true;
+    private int thingy = 0;
 
     void Start()
     {
@@ -37,7 +38,7 @@ public class Transmit : MonoBehaviour
                 while (client.Connected)
                 {
                     // Keep this thread alive as long as the client is connected
-                    Thread.Sleep(100);
+                    Thread.Sleep(10);
                 }
                 if (!isRunning) break;
                 Debug.Log("Client disconnected. Waiting for new connection...");
@@ -66,9 +67,28 @@ public class Transmit : MonoBehaviour
             {
                 try
                 {
-                    double xAxis = gameObject.transform.localRotation.eulerAngles.x;
-                    double yAxis = gameObject.transform.localRotation.eulerAngles.y;
-                    byte[] data = Encoding.ASCII.GetBytes($"{Math.Round(xAxis, 2)} {Math.Round(yAxis, 2)}\n");
+                    float xAxis = gameObject.transform.eulerAngles.x;
+                    double yAxis = gameObject.transform.eulerAngles.y;
+                    double zAxis = gameObject.transform.eulerAngles.z;
+                    thingy += 1;
+                    string thing = "";
+                    if (thingy > 2)
+                    {
+                        thingy = 0;
+                    }
+                    switch (thingy)
+                    {
+                        case 0:
+                            thing = $"Y{Math.Round(xAxis, 2)}" + '\r';
+                            break;
+                        case 1:
+                            thing = $"P{Math.Round(yAxis, 2)}" + '\r';
+                            break;
+                        case 2:
+                            thing = $"R{Math.Round(zAxis, 2)}" + '\r';
+                            break;
+                    }
+                    byte[] data = Encoding.ASCII.GetBytes($"R{zAxis}" + '\r' + $"Y{xAxis}" + '\r' + $"P{yAxis}" + '\r');
                     stream.Write(data, 0, data.Length);
                 }
                 catch (System.Exception e)
@@ -76,7 +96,7 @@ public class Transmit : MonoBehaviour
                     Debug.Log("Failed to send data: " + e.Message);
                 }
             }
-            yield return new WaitForSeconds(0.05f); // Send data every 100ms
+            yield return new WaitForSeconds(0.1f); // Send data every 100ms
         }
     }
 
